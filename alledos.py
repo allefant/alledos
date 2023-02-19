a10c8d7d4e0cf0cd4ccb80f8e2ce8be90053d6b0
Allefant <allefant@allefant.com>
2023-02-19 18:42:08 -0500

    add alledos.py

---
+++ alledos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from discord import Client, Intents
+from configparser import ConfigParser
+from plugins import allegro5
+allegro5.basedir = "/home/allebot/discord"
+
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("? "):
+        name = message.content[2:]
+        result = allegro5.run(name, None)
+        if result:
+            if not result[0].startswith("http"):
+                result[0] = "`" + result[0].replace(" (", "(") + "`"
+            await message.channel.send("\n".join(result))
+
+c = ConfigParser()
+c.read("/etc/alledos.ini")
+client.run(c["discord"]["token"])
+
