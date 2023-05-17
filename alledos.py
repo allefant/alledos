919f60300efc1c89b67c6e6a62aa2c5d50d1147d
Allefant <allefant@allefant.com>
2023-05-17 07:10:49 -0400

    enable tins plugin

--- alledos.py
+++ alledos.py
@@ -3,8 +3,10 @@ from discord import Client, Intents
 from configparser import ConfigParser
 import sys
 sys.path.append("/home/allebot/discord")
+sys.path.append("/home/allebot/discord/plugins")
 from plugins import allegro5
 allegro5.basedir = "/home/allebot/discord"
+from plugins import tins
 
 intents = Intents.default()
 intents.message_content = True
@@ -17,7 +19,12 @@ async def on_message(message):
 
     if message.content.startswith("? "):
         name = message.content[2:]
-        result = allegro5.run(name, None)
+        if name == "tins":
+            result = tins.run("", None)
+        elif name in ["tins entrants", "tins *"]:
+            result = tins.run(name[len("tins "):], None)
+        else:
+            result = allegro5.run(name, None)
         if result:
             if not result[0].startswith("http"):
                 result[0] = "`" + result[0].replace(" (", "(") + "`"
